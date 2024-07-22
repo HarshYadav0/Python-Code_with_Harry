11c49fd341f182f618875020f26bdb19bd06cad0
Harsh Yadav <91417647+HarshYadav0@users.noreply.github.com>
2024-07-22 17:12:29 +0530

    Add files via upload

---
+++ Project- Jarvis/client.py
@@ -0,0 +1,6 @@
+
+
+#pip install openai
+
+# defaults to getting the key using os.environ.get("OPENAI_API_KEY")
+# if you saved the key under a different environment variable name, you can do something like:
---
+++ Project- Jarvis/musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "stealth" : "https://youtu.be/f_9NLcyAblY?si=EYpVKAj9LLbWRzlg",
+    "active" : "https://youtu.be/f_9NLcyAblY?si=EYpVKAj9LLbWRzlg",
+    "sajna" : "https://youtu.be/JRKk7gxcpMc?si=pcWj9LCnqW7hJv6k",
+    "aam jahe munde" : "https://youtu.be/muds1gFUTN8?si=aFwyY4K5L34scDk_",
+    "satisfya" : " https://youtu.be/pfVODjDBFxU?si=j2eJ_5Og58Z2gfBG"
+}
